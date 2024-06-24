f050cb36e3514998d26d8b368ef003db328ee9b7
galactic2005 <ariesbrandon99@gmail.com>
2024-06-23 22:10:19 -0500

    voices

---
+++ Utilities/voices.lua
@@ -0,0 +1,35 @@
+local voices = {
+    _AUTHORS = 'galactic_2005',
+    _VERSION = '1.0.0'
+}
+
+---Reloads the character voice to use their postfix, allowing dynamic reloading of the character voices
+---
+---The player or opponent sides are acceptable, girlfriend will result in the function doing nothing.
+---
+---If the current version of Psych Engine is 0.7.2 or lower, the function will do nothing.
+---@param characterName string
+function voices.reloadCharacterVoice(characterName)
+    if version < '0.7.3' then return end
+    assert(type(characterName) == 'string', 'Expected string for characterName, got ' .. type(characterName) .. '.') -- use only strings for characterName
+
+	local newCharacterName = stringTrim(characterName:lower())
+	local isGirlfriend = (newCharacterName == '2' or newCharacterName == 'gf' or newCharacterName == 'girlfriend')
+	if isGirlfriend then return end
+
+	local isDad = (newCharacterName == '1' or newCharacterName == 'dad' or newCharacterName == 'opponent')
+	local character = (isDad and 'dad' or 'boyfriend')
+
+    local backupVocalsFile = (isDad and 'Opponent' or 'Player')
+    local extraCode = (isDad and
+    'if(newVocals != null) opponentVocals.loadEmbedded(newVocals);' or
+    'vocals.loadEmbedded(newVocals != null ? newVocals : Paths.voices(song));')
+
+    runHaxeCode([[
+        var song = PlayState.SONG.song;
+        var newVocals = Paths.voices(song, (]] .. character .. [[.vocalsFile == null || ]] .. character .. [[.vocalsFile.length < 1) ? ']] .. backupVocalsFile .. [[' : ]] .. character .. [[.vocalsFile);
+    ]] .. extraCode
+    )
+end
+
+return voices
